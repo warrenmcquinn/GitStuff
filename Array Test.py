f1d5a913b7b6b2712403126a773fec8c146a7f86
Warren McQuinn <warren.mcquinn@gmail.com>
2015-01-02 13:27:42 -0800

    refactored in a for-loop

--- Array Test.py
+++ Array Test.py
@@ -1,8 +1,5 @@
 import Cryptsy
 
-counter = 0
-active = True
-
 #Logs in
 Exchange = Cryptsy.Cryptsy("1b17cb00a0e21c1b2dd063856edc4f3164b93107","51d77a60ab88afa1f67cb4ee1744c47120fd40960e99a43c8eac17bcc63da68bf6c64f8561fff71d")
 #Gets the market
@@ -10,19 +7,8 @@ market = Exchange.getMarkets()
 #Desired Coin
 coin = raw_input("Enter desired Coin: ")
 
-coin_list = []
+coin = unicode(coin)
 
-
-while(active):
-   addcoin = market['return'][counter]['primary_currency_name']
-   coin_list.append(addcoin)
-   counter = counter+1
-   if counter==350:
-        active=0
-   if addcoin == coin:
-      print market['return'][counter]
-
-#for item in coin_list:
-      #if item == coin:
-         #print item
-
+for currency in market['return']:
+	if currency['primary_currency_name'] == coin:
+		print currency
