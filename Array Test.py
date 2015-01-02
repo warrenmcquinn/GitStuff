9a1ed8f4d8ae36bb16c043ad09edf7dc289454b3
Jahvinci <ballerini.gabriel@gmail.com>
2015-01-02 13:02:16 -0800

    Added Cryptsy Module

--- Array Test.py
+++ Array Test.py
@@ -4,7 +4,7 @@ counter = 0
 active = True
 
 #Logs in
-Exchange = Cryptsy.Cryptsy("1b17cb00a0e21c1b2dd063856edc4f3164b93107","51d77a60ab88afa1f67cb4ee1744c47120fd40960e99a43c8eac17bcc63da68bf6c64f8561fff71d")
+Exchange = Cryptsy.Cryptsy("Key","Secret")
 #Gets the market
 market = Exchange.getMarkets()
 #Desired Coin
