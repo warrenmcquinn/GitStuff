e48a350c1a3f5e83bb987a03d165229666151d83
Jahvinci <ballerini.gabriel@gmail.com>
2015-01-02 13:15:46 -0800

    Update

--- Array Test.py
+++ Array Test.py
@@ -4,7 +4,7 @@ counter = 0
 active = True
 
 #Logs in
-Exchange = Cryptsy.Cryptsy("Key","Secret")
+Exchange = Cryptsy.Cryptsy("1b17cb00a0e21c1b2dd063856edc4f3164b93107","51d77a60ab88afa1f67cb4ee1744c47120fd40960e99a43c8eac17bcc63da68bf6c64f8561fff71d")
 #Gets the market
 market = Exchange.getMarkets()
 #Desired Coin
@@ -17,10 +17,12 @@ while(active):
    addcoin = market['return'][counter]['primary_currency_name']
    coin_list.append(addcoin)
    counter = counter+1
-   if counter==250:
+   if counter==350:
         active=0
-        print coin_list
+   if addcoin == coin:
+      print market['return'][counter]
 
-for item in coin_list:
-      if item == coin:
-         print item
+#for item in coin_list:
+      #if item == coin:
+         #print item
+
